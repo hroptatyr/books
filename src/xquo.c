f856c3186dfe8e292de35933a95e1f44d345a55c
Sebastian Freundt <freundt@ga-group.nl>
2017-07-13 05:04:44 +0000

    minor, externalise quote reading into read_xquo() of xquo.[ch]

---
+++ src/xquo.c
@@ -0,0 +1,175 @@
+/*** xquo.c -- quotes serialising/deserialising
+ *
+ * Copyright (C) 2016-2017 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
+ *
+ * This file is part of books.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **/
+#if defined HAVE_CONFIG_H
+# include "config.h"
+#endif	/* HAVE_CONFIG_H */
+#if defined HAVE_DFP754_H
+# include <dfp754.h>
+#endif	/* HAVE_DFP754_H */
+#include "dfp754_d32.h"
+#include "dfp754_d64.h"
+#include "xquo.h"
+#include "nifty.h"
+
+#define strtopx		strtod64
+#define pxtostr		d64tostr
+#define strtoqx		strtod64
+#define qxtostr		d64tostr
+
+#if defined __INTEL_COMPILER
+# pragma warning (push)
+# pragma warning (disable: 1419)
+# pragma warning (disable: 2203)
+#endif	/* __INTEL_COMPILER */
+
+extern void *memrchr(void const *s, int c_in, size_t n);
+#include "memrchr.c"
+
+#if defined __INTEL_COMPILER
+# pragma warning (pop)
+#endif	/* __INTEL_COMPILER */
+
+
+tv_t
+strtotv(const char *ln, char **endptr)
+{
+	char *on;
+	tv_t r;
+
+	/* time value up first */
+	with (long unsigned int s, x) {
+		if (UNLIKELY(!(s = strtoul(ln, &on, 10)) || on == NULL)) {
+			r = NOT_A_TIME;
+			goto out;
+		} else if (*on == '.') {
+			char *moron;
+
+			x = strtoul(++on, &moron, 10);
+			if (UNLIKELY(moron - on > 9U)) {
+				return NOT_A_TIME;
+			} else if ((moron - on) % 3U) {
+				/* huh? */
+				return NOT_A_TIME;
+			}
+			switch (moron - on) {
+			case 9U:
+				x /= MSECS;
+			case 6U:
+				x /= MSECS;
+			case 3U:
+				break;
+			case 0U:
+			default:
+				break;
+			}
+			on = moron;
+		} else {
+			x = 0U;
+		}
+		r = s * MSECS + x;
+	}
+out:
+	if (LIKELY(endptr != NULL)) {
+		*endptr = on;
+	}
+	return r;
+}
+
+xquo_t
+read_xquo(const char *line, size_t llen)
+{
+/* process one line */
+	char *lp, *on;
+	xquo_t q;
+
+	/* get timestamp */
+	if (UNLIKELY((q.t = strtotv(line, NULL)) == NOT_A_TIME)) {
+		return NOT_A_XQUO;
+	}
+
+	/* get qty */
+	if (UNLIKELY((lp = memrchr(line, '\t', llen)) == NULL)) {
+		/* can't do without quantity */
+		return NOT_A_XQUO;
+	}
+	llen = lp - line;
+	q.q.q = strtoqx(lp + 1U, NULL);
+
+	/* get prc */
+	if (UNLIKELY((lp = memrchr(line, '\t', llen)) == NULL)) {
+		/* can't do without price */
+		return NOT_A_XQUO;
+	}
+	llen = lp - line;
+	q.q.p = strtopx(lp + 1U, &on);
+	if (UNLIKELY(on <= lp + 1U)) {
+		/* invalidate price */
+		q.q.p = NANPX;
+	}
+
+	/* get flavour, should be just before ON */
+	with (unsigned char f = *(unsigned char*)--lp) {
+		/* map 1, 2, 3 to LVL_{1,2,3}
+		 * everything else goes to LVL_0 */
+		f ^= '0';
+		q.q.f = (typeof(q.q.f))(f & -(f < 4U));
+	}
+
+	/* rewind manually */
+	for (; lp > line && lp[-1] != '\t'; lp--);
+	with (unsigned char s = *(unsigned char*)lp) {
+		/* map A or a to ASK and B or b to BID
+		 * everything else goes to SIDE_UNK */
+		s &= ~0x20U;
+		s ^= '@';
+		q.q.s = (side_t)(s & -(s < NSIDES || s == 0x14U));
+
+		if (UNLIKELY(!q.q.s)) {
+			/* cannot put entry to either side, just ignore */
+			return NOT_A_XQUO;
+		}
+	}
+	llen = lp - line;
+
+	/* see if we've got pairs */
+	q.ins = memrchr(line, '\t', llen - 1U) ?: deconst(line - 1U);
+	q.ins++;
+	q.inz = lp - 1U - q.ins;
+	return q;
+}
+
+/* xquo.c ends here */
